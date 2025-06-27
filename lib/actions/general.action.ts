191a4b07b10df73515b7818a66a06f7373171e65
Mohandinkar <mohandinkar75@gmail.com>
2025-06-27 19:04:19 +0530

    solve error in feedback page

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -112,7 +112,6 @@ export async function getFeedbackByInterviewId(params: GetFeedbackByInterviewIdP
     const feedback = await db
         .collection('feedback')
         .where('interviewId', "==", interviewId)
-        .where('userId', "==", userId)
         .orderBy('createdAt', 'desc')
         .limit(1)
         .get()
